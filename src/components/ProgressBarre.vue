7d27735dfd0f752925acd1d5c7c67102ffdd90f0
sabbat10 <sabbatlumpatshia99@gmail.com>
2025-01-10 09:39:46 +0100

    Add progressBarre component

---
+++ src/components/ProgressBarre.vue
@@ -0,0 +1,16 @@
+<!-- tamplate -->
+<template>
+  <div>
+    <!-- <progress :value="value" :max="max"></progress> -->
+
+    Etape {{ value }} / {{ max }}
+  </div>
+</template>
+
+<!-- script -->
+<script setup>
+defineProps({
+  value: Number,
+  max: Number,
+});
+</script>
--- src/components/quiz.vue
+++ src/components/quiz.vue
@@ -1,11 +1,19 @@
 <!-- tamplate -->
 <template>
   <h1>{{ quiz.title }}</h1>
+  <ProgressBarre :value="step" :max="quiz.questions.length" />
 </template>
 
 <!-- script -->
 <script setup>
+import { ref } from "vue";
+// importation des composants
+import ProgressBarre from "./ProgressBarre.vue";
+
 defineProps({
   quiz: Object,
 });
+
+// variables
+const step = ref(0);
 </script>
