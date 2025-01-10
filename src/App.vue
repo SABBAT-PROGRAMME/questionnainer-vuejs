88f38eaf266b811b22959ce20b2a058affd4add7
sabbat10 <sabbatlumpatshia99@gmail.com>
2025-01-10 09:16:30 +0100

    add quiz component

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,49 @@
-<!--  -->
+<!-- tamplate -->
 <template>
-  <h1>Questionnaire</h1>
+  <!-- <h1>{{ quiz.title }}</h1> -->
+  <div v-if="state === 'error'">
+    <p>Impossible de charger quiz</p>
+    <!-- {{ quiz }} -->
+  </div>
+  <div :aria-busy="state === 'loading... '">
+    <Quiz :quiz="quiz" v-if="quiz" />
+  </div>
 </template>
+
+<!-- script -->
+<script setup>
+import { ref, onMounted } from "vue";
+
+// importation des composants
+import Quiz from "./components/quiz.vue";
+
+// variables
+const quiz = ref(null);
+const state = ref("loading... ");
+
+// mounted
+onMounted(() => {
+  fetch("/quiz.json")
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("impossible de récupérer les données");
+    })
+    .then((data) => {
+      quiz.value = data;
+      state.value = "idle";
+    })
+    .catch((error) => {
+      state.value = "error";
+    });
+});
+</script>
+
+<!-- style -->
+
+<style>
+.container {
+  margin-top: 2em;
+}
+</style>
---
+++ src/components/quiz.vue
@@ -0,0 +1,11 @@
+<!-- tamplate -->
+<template>
+  <h1>{{ quiz.title }}</h1>
+</template>
+
+<!-- script -->
+<script setup>
+defineProps({
+  quiz: Object,
+});
+</script>
