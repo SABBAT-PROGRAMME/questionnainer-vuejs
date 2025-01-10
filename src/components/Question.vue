45372278ed2bcbd2cf615c039539b09086961968
sabbat10 <sabbatlumpatshia99@gmail.com>
2025-01-10 10:07:03 +0100

    Create list in question

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -2,6 +2,16 @@
 <template>
   <div class="question">
     <h3>{{ question.question }}</h3>
+    <ul>
+      <li v-for="(choice, index) in question.choices" :key="choice">
+        <label :for="`answer-${index}`">
+          <input :id="`answer-${index}`" type="radio" name="answer" />{{
+            choice
+          }}</label
+        >
+      </li>
+    </ul>
+    <button>Question suivante</button>
   </div>
 </template>
 
@@ -16,5 +26,10 @@ const propos = defineProps({
 <style>
 .question {
   margin-top: 2rem;
+}
+
+.question button {
+  margin-left: auto;
+  display: block;
 }
 </style>
